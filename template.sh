2594e7510d9ce69f4639774385472b32ae0fc35d
KaustubhMestri <kaustubhmestri31@gmail.com>
2025-10-02 11:26:54 +0530

    Folder Structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+#create directory
+
+mkdir -p src
+mkdir -p research
+
+# create files 
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.py
+touch requirements.txt
+
+
+echo " Directory and Files created successfully !! "
