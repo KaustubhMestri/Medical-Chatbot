54a65f781d2b9c2ee6575e6ae01bdb4761b3c0b1
KaustubhMestri <kaustubhmestri31@gmail.com>
2025-10-02 11:59:31 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical_Chatbot",
+    version= "0.1.0",
+    author= " Kaustubh Mestri",
+    author_email="kaustubhmestri31@gmail.com"
+    packages= find_packages(),
+    install_requires = []
+)
