e84138bb517c2585d5673eef48a1f1e0e2913002
KaustubhMestri <kaustubhmestri31@gmail.com>
2026-05-30 22:44:33 +0530

    Refactor imports and improve code formatting in helper.py

--- src/helper.py
+++ src/helper.py
@@ -1,46 +1,41 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
 from typing import List
-from langchain.schema import Document
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_core.documents import Document
+
 
 def load_pdf_file(data):
-    loader = DirectoryLoader(
-        data,
-        glob="*.pdf",
-        loader_cls=PyPDFLoader
-    )
+    loader = DirectoryLoader(data, glob="*.pdf", loader_cls=PyPDFLoader)
     documents = loader.load()
     return documents
 
-def filter_to_minimal_docs(docs : List[Document]) -> List[Document]:
-    
-    minimal_docs : List[Document] = []
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+
+    minimal_docs: List[Document] = []
     for doc in docs:
         src = doc.metadata.get("source")
         minimal_docs.append(
-            Document(
-                page_content=doc.page_content,
-                metadata = {"source":src}
-            )
+            Document(page_content=doc.page_content, metadata={"source": src})
         )
     return minimal_docs
 
+
 def text_split(minimal_docs):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 200,
-        length_function = len
+        chunk_size=500, chunk_overlap=200, length_function=len
     )
     texts_chunk = text_splitter.split_documents(minimal_docs)
     return texts_chunk
 
+
 def download_embeddings():
 
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    embeddings = HuggingFaceEmbeddings(
-        model_name = model_name
-    )
+    embeddings = HuggingFaceEmbeddings(model_name=model_name)
     return embeddings
+
 
 embedding = download_embeddings()
